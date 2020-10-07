061e2e350bfc847834da17370b050c1539aa66d7
Your Name <you@example.com>
2020-10-07 20:05:27 +0530

    merged commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //test brnch merged master
         TestInterface t=new testClass();
         testClass a=new testClass();
         a.display();
